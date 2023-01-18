83e4c56bff1645743d03fe80f661c80023bdda36
PhilippJungemann <110348535+PhilippJungemann@users.noreply.github.com>
2023-01-18 19:50:04 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.db
    #	tracker.py

--- reminder.py
+++ reminder.py
@@ -2,11 +2,13 @@ from db import get_habit_data
 from datetime import date, datetime
 
 
-# reminder
+# Function for reminding the user about his current to-dos
 def reminder(db, name):
     all_data = get_habit_data(db, name)
+    # getting the last element of the data and convert it to a list
     last = all_data.pop()
     new = list(last)
+    # if the habit was not checked off at all, reminding the user of it
     if new[4] is None:
         print("Your habit " + name + " is waiting for the first check off!")
         return
@@ -14,12 +16,15 @@ def reminder(db, name):
     today = date.today()
     diff = today - last_check_off
     diff = int(diff.days)
+    # if the habit is a daily one and was not checked off today, it has to be done
     if new[2] == "daily":
         if diff > 0:
             print("Your habit '" + name + "' has to be done today!")
+        # if it already was checked off, there is no action required
         else:
             print("Great - your habit '" + name + "' doesn't need to be done until tomorrow!")
             return
+    # if the habit is a weekly one and was not checked off this week, it has to be done
     if new[2] == "weekly":
         last_check_off = datetime.strptime(new[4], '%Y-%m-%d').date()
         last_check_off = datetime.strftime(last_check_off, '%W')
@@ -27,9 +32,11 @@ def reminder(db, name):
         diff = int(current_week) - int(str(last_check_off))
         if abs(diff) > 0:
             print("Your habit '" + name + "' has to be done this week!")
+        # if it already was checked off, there is no action required
         else:
             print("Great - your habit '" + name + "' doesn't need to be done until next week!")
             return
+    # if the habit is a monthly one and was not checked off this month, it has to be done
     if new[2] == "monthly":
         last_check_off = datetime.strptime(new[4], '%Y-%m-%d').date()
         last_check_off = datetime.strftime(last_check_off, '%m')
@@ -37,9 +44,11 @@ def reminder(db, name):
         diff = int(current_month) - int(str(last_check_off))
         if abs(diff) > 0:
             print("Your habit '" + name + "' has to be done this month!")
+        # if it already was checked off, there is no action required
         else:
             print("Great - your habit '" + name + "' doesn't need to be done until next month!")
             return
+    # if the habit is a yearly one and was not checked off this year, it has to be done
     if new[2] == "yearly":
         last_check_off = datetime.strptime(new[4], '%Y-%m-%d').date()
         last_check_off = datetime.strftime(last_check_off, '%Y')
@@ -47,6 +56,7 @@ def reminder(db, name):
         diff = int(current_year) - int(str(last_check_off))
         if diff > 0:
             print("Your habit '" + name + "' has to be done this year!")
+        # if it already was checked off, there is no action required
         else:
             print("Great - your habit '" + name + "' doesn't need to be done until next year!")
             return
