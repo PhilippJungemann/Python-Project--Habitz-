4e697e7dce0c9a53dfd1631fda02db69cf91a552
PhilippJungemann <110348535+PhilippJungemann@users.noreply.github.com>
2023-01-19 17:16:39 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.db
    #	tracker.py

--- test_project.py
+++ test_project.py
@@ -1,45 +1,62 @@
+import pytest
 from habit import Habit
 #from db import get_db, add_counter, increment_counter, get_counter_data
 from analyse import calculate_count
 import questionary
 import time
 from datetime import date, datetime, timedelta
-#from db import get_db
-from habit import Habit
-#from analyse import calculate_count
-#from db import add_counter, increment_counter
+from db import get_db
+from analyse import calculate_count
+from db import add_counter, increment_counter
+
+
+def create_method():
+    db = get_db("test.db")
+    habit = Habit("Jogging", "I want to jog every day", "daily")
+    habit.store(db)
+    habit = Habit("Swimming", "I want to swim every week", "weekly")
+    habit.store(db)
+    habit = Habit("Hiking", "I want to hike every month", "monthly")
+    habit.store(db)
+    habit = Habit("Traveling", "I want to travel every year", "yearly")
+    habit.store(db)
+
+
+def generate_habit_data():
+    name = "Jogging"
+    db = get_db("test.db")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM habitz WHERE name =?", (name,))
+    data = cur.fetchall()
+
+#        increment_counter(db, "test_counter", "2021-12-06")
+#        increment_counter(db, "test_counter", "2021-12-07")
 #
+#        increment_counter(db, "test_counter", "2021-12-09")
+#        increment_counter(db, "test_counter", "2021-12-10")
 
-class TestCounter:
 
-    def setup_method(self):
-        self.db = get_db("test.db")
-        add_counter(self.db, "test_counter", "test_description")
-        increment_counter(self.db, "test_counter", "2021-12-06")
-        increment_counter(self.db, "test_counter", "2021-12-07")
+def test_db_counter():
+    counter = Counter("test_counter_1", "test_description_1")
+    counter.store(db)
 
-        increment_counter(self.db, "test_counter", "2021-12-09")
-        increment_counter(self.db, "test_counter", "2021-12-10")
+    counter.increment()
+    counter.add_event(db)
+    counter.reset()
+    counter.increment()
 
-    def test_db_counter(self):
-        counter = Counter("test_counter_1", "test_description_1")
-        counter.store(self.db)
 
-        counter.increment()
-        counter.add_event(self.db)
-        counter.reset()
-        counter.increment()
+def test_db_counter():
+    data = get_counter_data(db, "test_counter")
+    assert len(data) == 4
 
-    def test_db_counter(self):
-        data = get_counter_data(self.db, "test_counter")
-        assert len(data) == 4
+    count = calculate_count(db, "test_counter")
+    assert count == 4
 
-        count = calculate_count(self.db, "test_counter")
-        assert count == 4
 
-    def teardown_method(self):
-        import os
-        os.remove("test.db")
+def teardown_method():
+    import os
+    os.remove("test.db")
 
 
 
